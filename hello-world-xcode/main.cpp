987fa07686073150272519b297b0785b58fdec84
Geoffray Adde <geoffray.adde@sonarsource.com>
2020-01-10 15:24:18 +0100

    init commit

---
+++ hello-world-xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello-world-xcode
+//
+//  Created by Geoffray Adde on 10/01/2020.
+//  Copyright © 2020 Geoffray Adde. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
